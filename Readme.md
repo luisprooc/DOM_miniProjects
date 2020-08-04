95dbd9882f6e71eba764678616b5f22721b90d37
Luis Miguel Rosario <luisprooc@gmail.com>
2020-08-04 17:31:52 -0500

    Se inicio la creacion del proyecto tweets junto con su descripcion, se agrego la funcion de agregar los tweets a la lista.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,16 @@
 # Javascript DOM Projects 
 
+*Note: By the way the HTML and CSS will be predefined*
+======================================================
+
 ## Tweets from a textarea
+
+_This is a small project using local storage in which:_
+
+*The user can create/ delete/ see his tweets in a list.
+
+*Tweets will be stored in local storage.
+
+*The user can add a unlimited amount of tweets.
+
+*When loading the page the tweets must be loaded from the local storage and when deleted they must be removed from said place.
---
+++ proyecto_misTweets/js/app.js
@@ -0,0 +1,44 @@
+
+// Variables
+
+const listaTweets = document.getElementById("lista-tweets");
+
+
+// Events Listeners
+
+eventos();
+
+function eventos(){
+    document.querySelector("#formulario").addEventListener("submit",agregarTweet);
+}
+
+
+
+
+
+// Funciones
+
+// añadir tweet del formulario
+
+function agregarTweet(e){
+    e.preventDefault();
+
+    // Tener valor del textarea
+    let tweet = document.getElementById("tweet").value;
+
+    // crear boton de eliminar
+    let botonBorrar = document.createElement("a");
+    botonBorrar.classList.add("borrar-tweet");
+    botonBorrar.innerText = "X";
+
+    // crear elemento y añadirle el texto
+
+    let li = document.createElement("li");
+    li.innerText = tweet;
+    // Añade el boton de borrar al tweet
+
+    li.appendChild(botonBorrar);
+    // Añade tweets a la lista
+    listaTweets.appendChild(li);
+
+}
