6a8a2bf1691511d5358a835e8036332c8c55fd35
Luis Miguel Rosario <luisprooc@gmail.com>
2020-08-06 16:56:27 -0500

    Funcionalidad de agregar cursos al carrito terminada, se uso templates iterals

--- proyecto_carrito/js/app.js
+++ proyecto_carrito/js/app.js
@@ -4,6 +4,7 @@ const carrito = document.getElementById("carrito");
 
 const cursos = document.getElementById("lista-cursos");
 
+const listaCursos = document.querySelector("#lista-carrito tbody");
 
 
 
@@ -35,5 +36,30 @@ function comprarCurso(e){
 // Leer los datos del curso
 
 function leerDatosCurso(curso){
-    console.log(curso);
+    const infoCurso = {
+        imagen: curso.querySelector("img").src,
+        titulo: curso.querySelector("h4").textContent,
+        precio: curso.querySelector(".precio span").textContent,
+        id: curso.querySelector("a").getAttribute("data-id")
+    }
+    insertarCarrito(infoCurso);
+}
+
+
+// Muestra el curso seleccionado en el carrito
+
+function insertarCarrito(infoCurso){
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>
+            <img src ="${infoCurso.imagen}" width =100>
+        </td>
+        <td> ${infoCurso.titulo} </td>
+        <td> ${infoCurso.precio} </td>
+        <td> 
+            <a href"#" class="borrar-curso" data-id="${infoCurso.id}"
+            >X</a>
+        </td>
+    `;
+    listaCursos.appendChild(row);
 }
