3561a5953425e4a1a9175eb5d5f0edbd566174ab
Luis Miguel Rosario <luisprooc@gmail.com>
2020-08-08 16:48:11 -0500

    Funciones de validar los campos creada

--- proyecto_email/js/app.js
+++ proyecto_email/js/app.js
@@ -15,6 +15,12 @@ function eventos(){
 
     // Cuando cargue la pagina
     document.addEventListener("DOMContentLoaded",inicioApp);
+
+    // Campos del formulario
+
+    email.addEventListener("blur",validarCampo);
+    asunto.addEventListener("blur",validarCampo);
+    mensaje.addEventListener("blur",validarCampo);
 }
 
 
@@ -24,4 +30,52 @@ function eventos(){
 
 function inicioApp(){
     btnEnviar.disabled = true;
+}
+
+
+// Verificar si en el campo hay algo
+
+function validarCampo(){
+    
+    // Se validad la longitud del texto
+
+    validarLongitud(this);
+
+    // validar el email
+
+    if(this.type === "email"){
+        validarEmail(this);
+    }
+
+    let errores = document.querySelectorAll(".error");
+    if(email.value != "" && asunto.value != "" && mensaje.value != ""){
+        if(errores.length === 0){
+            btnEnviar.disabled = false;
+        }
+        
+    }
+}
+
+function validarLongitud(campo){
+    if(campo.value.length > 0){
+        campo.style.borderBottomColor = "green";
+        campo.classList.remove("error");
+    }
+
+    else{
+        campo.style.borderBottomColor = "red";
+        campo.classList.add("error");
+    }
+}
+
+
+// Validar email
+
+function validarEmail(campo){
+    const mensaje = campo.value;
+    
+    if(mensaje.indexOf("@") === -1 || mensaje.indexOf(".com") === -1 ){
+        campo.style.borderBottomColor = "red";
+        campo.classList.add("error");
+    }
 }
