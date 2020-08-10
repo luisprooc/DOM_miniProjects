974c9b934a2e34c6e13ca15b17dfb8aa64f6669e
Luis Miguel Rosario <luisprooc@gmail.com>
2020-08-10 17:03:55 -0500

    Se agrego el constructor del seguro y la interfaz, ademas ya podemos obtener los datos de los campos.

--- proyecto_cotizaSeguro/js/app.js
+++ proyecto_cotizaSeguro/js/app.js
@@ -1,8 +1,54 @@
+// Constructores
+
 // Cotizaor constructor
 
-function Seguro(){
-
+function Seguro(marca,anio,tipo){
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
 }
+
+// Todo lo que se muestra
+
+function Interfaz(){}
+
+
+// Eventos
+
+const formulario =document.getElementById("cotizar-seguro");
+
+formulario.addEventListener("submit",function(e){
+    e.preventDefault();
+
+    // Obtener marca seleccionada
+    const marca = document.getElementById("marca");
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+    // Obtener año seleccionada
+    const anio = document.getElementById("anio");
+    const anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+    const tipo = document.querySelector("input[name=tipo]:checked").value;
+
+    const interfaz = new Interfaz();
+
+    // Revisamos si los campos estan vacios
+
+    if(marcaSeleccionada === ""){
+        console.warn("faltan datos");
+    }
+
+    else{
+        console.log("XD");
+    }
+});
+
+
+
+
+
+
+// Crear opciones del campo anios
 
 const max = new Date().getFullYear(),
     min = max - 20;
