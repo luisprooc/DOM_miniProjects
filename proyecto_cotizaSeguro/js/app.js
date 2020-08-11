75187ebfb9ccee4940fcb699e90661d77d256976
Luis Miguel Rosario <luisprooc@gmail.com>
2020-08-11 17:03:15 -0500

    Se agrego la funcionalidad de calcular el seguro dependiendo su tipo, modelo y anio.

--- proyecto_cotizaSeguro/js/app.js
+++ proyecto_cotizaSeguro/js/app.js
@@ -8,6 +8,55 @@ function Seguro(marca,anio,tipo){
     this.tipo = tipo;
 }
 
+
+Seguro.prototype.cotizarSeguro = function(){
+    /*
+        1 = americano 1.15
+        2 = asiatico 1.05
+        3 = europeo 1.35
+
+    */
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca){
+        case "1":
+            cantidad = base * 1.15;
+            break;
+
+        case "2":
+            cantidad = base * 1.05;
+            break;
+        
+        default:
+            cantidad = base * 1.35;
+            break;
+
+    }
+
+    // leer el año
+
+    const diferencia = new Date().getFullYear() - this.anio;
+    
+    // cada año de diferencia hay que reducir en un 3%
+
+    cantidad -= (diferencia * 0.03) * cantidad;
+
+    // Si el seguro es basico se multiplica por 30%
+    // Si es completo por 50%
+
+    if(this.tipo === "basico"){
+        cantidad *= 1.30; 
+    }
+
+    else{
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
+}
+
+
 // Todo lo que se muestra
 
 function Interfaz(){}
@@ -60,7 +109,13 @@ formulario.addEventListener("submit",function(e){
     }
 
     else{
-        console.log("Correcto","correcto");
+        // Crear el seguro
+        const seguro = new Seguro(marcaSeleccionada,anioSeleccionado,tipo);
+
+        // cotizar
+
+        const monto = seguro.cotizarSeguro();
+        
     }
 });
 
