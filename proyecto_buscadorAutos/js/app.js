9cb9cfdb6f811d8b2e90937073f2832c7ac4a8a9
Luis Miguel Rosario <luisprooc@gmail.com>
2020-08-27 15:15:08 -0500

    Se agrego la funcion de generar los years

--- proyecto_buscadorAutos/js/app.js
+++ proyecto_buscadorAutos/js/app.js
@@ -2,12 +2,22 @@
 
 const resultado = document.getElementById("resultado");
 
+const year = document.getElementById("year");
+
+// obtener años
+const max = new Date().getFullYear(); 
+const min = max - 5;
 
 
 // eventos
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    // muestra los autos al cargar
     mostrarAutos();
+
+    // llenar años del select
+
+    llenarYears();
 });
 
 
@@ -18,6 +28,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 function mostrarAutos(){
     autos.forEach(auto => {
         
+        // destructuring
         const {marca, modelo, year, puertas, transmision, precio, color} = auto;
         const html = document.createElement("p");
 
@@ -27,4 +38,20 @@ function mostrarAutos(){
         resultado.appendChild(html);
     });
 
+}
+
+
+function llenarYears(){
+
+    // Recorrer de adelante hacia atras los años
+    for(let i = max; i >= min;--i ){
+        const opcion = document.createElement("option");
+
+        opcion.value = i;
+
+        opcion.textContent = i;
+
+        // Agregar la opcion al select
+        year.appendChild(opcion);
+    }
 }
